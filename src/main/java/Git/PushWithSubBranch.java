ecc58be9661ab813c60e83309e55be3fce747882
kaller_axe <ashirbaev010399@gmail.com>
2023-01-30 15:46:12 +0200

    Fix My Changes in Branch

---
+++ src/main/java/Git/PushWithSubBranch.java
@@ -0,0 +1,7 @@
+package Git;
+
+public class PushWithSubBranch {
+    public static void main(String[] args) {
+        System.out.println("System print Sub Branch Name");
+    }
+}
--- src/main/java/JustNow/afterMerge.java
+++
@@ -1,7 +0,0 @@
-package JustNow;
-
-public class afterMerge {
-    public static void main(String[] args) {
-        System.out.println("Hello");
-    }
-}
